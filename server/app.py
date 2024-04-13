1da87ae2be87154781667503ac7fff404abaaf61
Priscilla-22 <priscillamw123@gmail.com>
2024-04-13 10:34:14 +0300

    feat:Authenticating Users

--- server/app.py
+++ server/app.py
@@ -18,20 +18,22 @@ db.init_app(app)
 
 api = Api(app)
 
+
 class ClearSession(Resource):
 
     def delete(self):
-    
         session['page_views'] = None
         session['user_id'] = None
 
         return {}, 204
 
+
 class IndexArticle(Resource):
-    
+
     def get(self):
         articles = [article.to_dict() for article in Article.query.all()]
         return articles, 200
+
 
 class ShowArticle(Resource):
 
@@ -40,7 +42,6 @@ class ShowArticle(Resource):
         session['page_views'] += 1
 
         if session['page_views'] <= 3:
-
             article = Article.query.filter(Article.id == id).first()
             article_json = jsonify(article.to_dict())
 
@@ -48,10 +49,45 @@ class ShowArticle(Resource):
 
         return {'message': 'Maximum pageview limit reached'}, 401
 
+
+class Login(Resource):
+
+    def post(self):
+        username = request.get_json().get('username')
+        user = User.query.filter(User.username == username).first()
+
+        if user:
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+
+        return {}, 401
+
+
+class Logout(Resource):
+
+    def delete(self):
+        session['user_id'] = None
+
+        return {}, 204
+
+
+class CheckSession(Resource):
+
+    def get(self):
+        user_id = session['user_id']
+        if user_id:
+            user = User.query.filter(User.id == user_id).first()
+            return user.to_dict(), 200
+
+        return {}, 401
+
+
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 
-
-if __name__ == '__main__':
+if __name__ == '_main_':
     app.run(port=5555, debug=True)
